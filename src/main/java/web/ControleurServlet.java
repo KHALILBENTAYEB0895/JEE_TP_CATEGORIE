805e34f3e7cb4b82cbe2d7b1ab1fd50610cfffc4
KalilBENTAYEB <khalil.bentayeb95@gmail.com>
2024-02-09 23:47:27 +0000

    Recheche DONE

---
+++ src/main/java/web/ControleurServlet.java
@@ -0,0 +1,45 @@
+package web;
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import metier.entities.Produit;
+
+import java.io.IOException;
+import java.util.List;
+
+import dao.IProduitDao;
+import dao.ProduitDaoImpl;
+
+
+public class ControleurServlet extends HttpServlet {
+
+	private IProduitDao metier;
+
+	@Override
+	public void init() throws ServletException {
+		metier = new ProduitDaoImpl();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		String path = request.getServletPath();
+		if(path.equals("/index.do")) {
+			request.getRequestDispatcher("produits.jsp").forward(request, response);
+		}
+		else if(path.equals("/chercher.do")) {
+			String motCle = request.getParameter("motCle");
+			ProduitModel model = new ProduitModel();
+			model.setMotCle(motCle);
+			List<Produit> produits = metier.produitsParMC(motCle);
+			model.setProduits(produits);
+			request.setAttribute("model", model);
+			request.getRequestDispatcher("produits.jsp").forward(request, response);
+		}
+	}
+	
+}
---
+++ src/main/webapp/produits.jsp
@@ -0,0 +1,43 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Produits</title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"/>
+	</head>
+	<body>
+		<div class="container">
+			<div class="card border-primary mb-3">
+		      <div class="card-header text-bg-primary">Liste des produits</div>
+		      <div class="card-body">
+		        <form action="chercher.do" method="get">
+		       		<label class="fw-bold">Mot Cle:</label>
+		       		<input type="text" name="motCle"/>
+		       		<button type="submit" class="btn btn-primary">Chercher</button>
+		        </form>
+		        <table class="table">
+				  <thead>
+				    <tr>
+				      <th scope="col">ID</th>
+				      <th scope="col">Designation</th>
+				      <th scope="col">Prix</th>
+				      <th scope="col">Quantite</th>
+				      <th scope="col">Operation</th>
+				    </tr>
+				  </thead>
+				  <tbody>
+				   <c:forEach items = "${model.produits}" var="p">
+				   	<tr>
+				   		<td>${p.id}</td>
+				   		<td>${p.designation}</td>
+				   		<td>${p.prix}</td>
+				   		<td>${p.quantite}</td>
+				   	</tr>
+				   </c:forEach> 
+				  </tbody>
+				</table>
+		      </div>
+		</div>
+	</body>
+</html>
